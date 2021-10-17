dbb9e25d3bb079b25f09e351af072ca14fdcf9d0
Sukhpreet Singh <45624656+singh-sukhpreet@users.noreply.github.com>
2021-10-17 18:31:35 -0400

    Update testSidebar.js

--- test/testSidebar.js
+++ test/testSidebar.js
@@ -10,20 +10,20 @@ describe('Sidebar Test Cases', () => {
         browser.maximizeWindow();
     });
     
-    describe('As a customer, I can access Sidebar Page', () => {
+    it('As a customer, I can submit PERSONAL SHOPPER request', ()=> {
+        browser.pause(500);
+        browser.switchToFrame(pageFrame.frameSidebar);
+        pageFrame.frameSidebarWidgetLocation.waitForDisplayed();
+        pageFrame.frameSidebarWidgetLocation.click();
+        pageFrame.selectSecondaryWindow()
+        pageFrame.frameLinkPS.click();
+        pageFrame.makePersonalShopperRequest();
+        expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
+        browser.switchToFrame(null);
+    });
 
-        it('As a customer, I can submit PERSONAL SHOPPER request from sidebar', ()=> {
-            browser.pause(500);
-            browser.switchToFrame(pageFrame.frameSidebar);
-            pageFrame.frameSidebarWidgetLocation.waitForDisplayed();
-            pageFrame.frameSidebarWidgetLocation.click();
-            pageFrame.selectSecondaryWindow()
-            pageFrame.frameLinkPS.click();
-            pageFrame.makePersonalShopperRequest();
-            expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
-            browser.switchToFrame(null);
-        });
-
+    after(() => {
+        browser.closeWindow();
     });
 
 });
