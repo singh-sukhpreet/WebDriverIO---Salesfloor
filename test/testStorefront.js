d2091d3e41768d0b3bcdfedce9088841689d8258
Sukhpreet Singh <45624656+singh-sukhpreet@users.noreply.github.com>
2021-10-17 18:31:01 -0400

    Update testStorefront.js

--- test/testStorefront.js
+++ test/testStorefront.js
@@ -3,42 +3,46 @@ const pageFrame = require('../pages/pageFrame');
 const ResourceInput = require('../resources/resourceInput');
 const expectChai = require('chai').expect;
 
-describe('Storefront Test Cases', () => {
-    
+describe('Storefront - Test Cases', () => {
+
     before(() => {
         browser.url(pageStorefront.storeFrontUrl);
         browser.maximizeWindow();
     });
     
-    describe(`As a customer, I can access Storefront (${pageStorefront.storeFrontUrl})`, () => {
+    it(`As a customer, I see the "${ResourceInput.liveChatText}" status as "${ResourceInput.unavailableText}"`, () => {
+        pageStorefront.repChatStatus.waitForDisplayed();
+        expect(pageStorefront.repChatStatus).toHaveTextContaining(ResourceInput.unavailableText);
+        browser.takeScreenshot();
+    });
 
-        it(`As a customer, I see the "${ResourceInput.liveChatText}" status as "${ResourceInput.unavailableText}"`, () => {
-            pageStorefront.repChatStatus.waitForDisplayed();
-            expect(pageStorefront.repChatStatus).toHaveTextContaining(ResourceInput.unavailableText);
-        });
+    it('As a customer, I can Verify that the text colour of Live chat is red when rep is not available', () => {
+        pageStorefront.repChatStatus.waitForDisplayed();
+        let colorValueInHex = (pageStorefront.repChatStatus.getCSSProperty('color'))['parsed']['hex'];
+        expectChai(colorValueInHex).to.equal(ResourceInput.unavailableTextColor);
+        browser.takeScreenshot();
+    })
 
-        it('As a customer, I can Verify that the text colour of Live chat is red when rep is not available', () => {
-            pageStorefront.repChatStatus.waitForDisplayed();
-            let colorValueInHex = (pageStorefront.repChatStatus.getCSSProperty('color'))['parsed']['hex'];
-            expectChai(colorValueInHex).to.equal(ResourceInput.unavailableTextColor);
-        })
+    it('As a customer, I can sign up for regular updates', () => {
+        pageStorefront.linkGetMyUpdates.waitForDisplayed();
+        pageStorefront.linkGetMyUpdates.click();
+        pageFrame.makeGetMyUpdatesRequest();
+        expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
+        browser.switchToFrame(null);
+        browser.takeScreenshot();
+    });
 
-        it('As a customer, I can sign up for regular updates', () => {
-            pageStorefront.linkGetMyUpdates.waitForDisplayed();
-            pageStorefront.linkGetMyUpdates.click();
-            pageFrame.makeGetMyUpdatesRequest();
-            expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
-            browser.switchToFrame(null);
-        });
-
-        it('As a customer, I can submit an appointment request', ()=> {
-            pageStorefront.linkAppointmentRequest.waitForDisplayed();
-            pageStorefront.linkAppointmentRequest.click();
-            pageFrame.makeBookAnAppointmentRequest();
-            expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
-            browser.switchToFrame(null);
-        });
-
+    it('As a customer, I can submit an Appointment request', ()=> {
+        pageStorefront.linkAppointmentRequest.waitForDisplayed();
+        pageStorefront.linkAppointmentRequest.click();
+        pageFrame.makeBookAnAppointmentRequest();
+        expect(pageFrame.frameThankYouText).toHaveTextContaining(ResourceInput.thankYouText);
+        browser.switchToFrame(null);
+        browser.takeScreenshot();
+    });
+    
+    after(() => {
+        browser.closeWindow();
     });
 
 });
